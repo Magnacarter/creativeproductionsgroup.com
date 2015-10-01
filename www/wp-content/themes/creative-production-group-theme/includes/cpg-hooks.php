2afd27b227a6c545e17c968cf0e891bde10aa9b5
Adam.Carter <adamkristopher@gmail.com>
2015-10-01 02:48:54 -0500

    video homepage

--- www/wp-content/themes/creative-production-group-theme/includes/cpg-hooks.php
+++ www/wp-content/themes/creative-production-group-theme/includes/cpg-hooks.php
@@ -4,6 +4,30 @@
  */
 
 /**
+ * Hook in video components
+ *
+ *
+ *
+ */
+function cpg_video_lightbox() {
+	if ( is_front_page() ) {
+		?>
+			<div class="x">
+				<img src="https://creativeproductionsgroup.com/wp-content/uploads/2015/03/x.png" height="35" width="35"></img>
+			</div>
+
+			<div id="vimeoWrap">
+				<div id="video-container">
+					<h1>hello world</h1>
+					<?php the_field( 'watch_video' ) ?>
+				</div>
+			</div>
+		<?php
+	}
+}
+add_action( 'wp_head', 'cpg_video_lightbox' );
+
+/**
  * Customize credits in the footer
  *
  * @filter genesis_footer_creds_text
--- www/wp-content/themes/creative-production-group-theme/js/theme.js
+++ www/wp-content/themes/creative-production-group-theme/js/theme.js
@@ -10,4 +10,31 @@ jQuery( document ).ready( function($) {
 			animation: "fade", //String: Select your animation type, "fade" or "slide"
 		});
 	});
+
+//Scroll to next element of HP via the 'Learn More' button.
+	$( '.learn' ).click( function() {
+		$( 'html, body' ).animate( {
+			//the minus 105 makes it not scroll all the way down to the target.
+			scrollTop: $( '#front-page-2' ).offset().top + 55
+		}, 1000);
+	});
+
+//HP video to lightbox via the 'watch video' button.
+	var watch = $( '.watch' );
+	watch.click( function() {
+		froogaloop = $f( '#cpg-video' );
+		froogaloop.api('play');
+		$( '.site-inner' ).append( '<div id="wrapper"></div>' );
+		$( '#wrapper' ).show();
+		$( '#video-container' ).show();
+		$( '.x' ).show();
+	});
+	$( '.x' ).click( function() {
+		$( '#wrapper' ).hide();
+		$( '#video-container' ).hide();
+		$( '.x' ).hide();
+		vimeoWrap = $('#vimeoWrap');
+   		vimeoWrap.html( vimeoWrap.html() );
+	});
+
 });//Document ready end.
